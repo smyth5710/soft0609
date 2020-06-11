ec4a42403aa0aa69b2d266a9748dce187878a8cf
smyth5710 <1807213390@qq.com>
2020-06-11 15:50:05 +0800

    music

--- App.js
+++ App.js
@@ -1,135 +1,87 @@
 import React, { Component } from 'react'
-import { Text, View, Button,Modal } from 'react-native'
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer, StackActions } from '@react-navigation/native';
-import ViewPager from '@react-native-community/viewpager';
-import AntDesign from "react-native-vector-icons/AntDesign"
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import { Text, View, Button } from 'react-native'
+import Video from 'react-native-video';
+import Slider from '@react-native-community/slider'
 
 
-const Drawer=createDrawerNavigator()
-const Tab=createBottomTabNavigator()
-const Stack=createStackNavigator()
+
+const songs=["许嵩-雨幕.mp3","许嵩-如果当时.mp3","许嵩-半城烟沙.mp3"]
+const url="http://47.95.10.109:8000/"
+var p=0
 export default class App extends Component {
   constructor(props){
     super(props)
-    this.state={visible:true}
+    this._player=null
+    this.state={paused:true,song:songs[p],source:{uri:url+songs[p]}
+  ,duration: 1,currentTime:0
+  
   }
-  _hide=()=>{
-    this.setState({visible:false})
+    
+  }
+  _playControle=()=>{
+    let paused=this.state.paused
+    this.setState({paused:!paused})
+  
+  }
+
+  _next=()=>{
+    p++
+    this.setState({source:{uri:url+songs[p]},song:songs[p]})
+  }
+  
+  _prev=()=>{
+    p--
+    this.setState({source:{uri:url+songs[p]},song:songs[p]})
+  }
+
+  _loadHandler=({duration})=>{
+      this.setState({duration})
+
 
   }
-  render() {
+
+  _progressHandler=({currentTime})=>{
+    this.setState({currentTime})
+
+
+  }
+  _seekHandler=value=>{
+    this._player.seek(value)
+
+  }
+
+  _endHandler=()=>{
+    this._next()
+  }
+
+  render() { 
     return (
-     <NavigationContainer>
-       <Modal visible={this.state.visible}>
-         <ViewPager style={{flex:1}} initialPage={0}>
-         <View key="1">
-          <Text>First page</Text>
-          </View>
-          <View key="2">
-          <Text>Second page</Text>
-          <Button title="关闭" onPress={this._hide}/>
-          </View>
-           </ViewPager>
-       </Modal>
-        <Drawer.Navigator>
-          <Drawer.Screen name="Main" component={Main}/>
-          <Drawer.Screen name="Setting" component={Setting}/>
-        </Drawer.Navigator>
-     </NavigationContainer>
+      <View style={{flex:1,justifyContent:'center'}}>
+        <Video 
+        ref={ref=>this._player=ref}
+        source={this.state.source}
+        paused={this.state.paused}
+        onLoad={this._loadHandler}
+        onProgress={this._progressHandler}
+        onEnd={this._endHandler}
+        progressUpdateInterval={2000}
+        />
+        
+
+
+        <Text style={{fontSize:20,textAlign:'center'}}>{this.state.song}</Text>
+        <Slider style={{height:100}} minimumValue={0} 
+        maximumValue={this.state.duration} 
+        onSlidingComplete={this._seekHandler}
+        value={this.state.currentTime}/>
+
+
+        <View style={{flexDirection:'row-reverse',justifyContent:'space-between'}}>
+        <Button title="上一首" onPress={this._prev}/>
+        <Button title="播放/暂停" onPress={this._playControle}/>
+        <Button title="下一首" onPress={this._next}/>
+        </View>
+      </View>
     )
-  }
-}
-
-class Main extends React.Component{
-
-render(){
-  return <Tab.Navigator
-  screenOptions={({ route }) => ({
-    tabBarIcon: ({ focused, color, size }) => {
-      let iconName;
-
-      if (route.name === 'Home') {
-        iconName = focused
-          ? 'ios-information-circle'
-          : 'ios-information-circle-outline';
-      } else if (route.name === 'Buy') {
-        iconName = focused ? 'ios-list-box' : 'ios-list';
-      }else if (route.name === 'FAQ') {
-        iconName = focused ? 'logo-google' : 'logo-googleplus';
-      }
-
-      // You can return any component that you like here!
-      return <Ionicons name={iconName} size={size} color={color} />;
-    },
-  })}
-  tabBarOptions={{
-    activeTintColor: 'tomato',
-    inactiveTintColor: 'gray',
-  }}
-  >
-
-    <Tab.Screen name="Home" component={Home}/>
-    <Tab.Screen name="Buy" component={Buy}/>
-    <Tab.Screen name="FAQ" component={FAQ}/>
-  </Tab.Navigator>
-
- }
-}
-
-class Setting extends React.Component{
-
-  render(){
-  return  <Text>Setting</Text>
-  }
-}
-
-class Home extends React.Component{
-
-  render(){
-  return (<View>
-    <AntDesign name="stepbackward" size={60} color="red"/>
-    <Text>Main</Text>
-  </View> )
-  }
-}
-
-class Buy extends React.Component{
-
-  render(){
-  return  <Stack.Navigator>
-  <Stack.Screen name="ItemList"component={ItemList}/>
-  <Stack.Screen name="ItemDetail"component={ItemDetail}/>
-</Stack.Navigator>
-  }
-}
-
-class FAQ extends React.Component{
-
-  render(){
-  return  <Text>FAQ</Text>
-  }
-}
-
-class ItemList extends React.Component{
-  _showDetail=()=>{
-    this.props.navigation.navigate("ItemDetail")
-  }
-
-  render(){
-  return  <View>
-  <Text>ItemList</Text>
-  <Button title="查看详情" onPress={this._showDetail}/>
-  </View>
-  }
-}
-
-class ItemDetail extends React.Component{
-
-  render(){
-  return  <Text>ItemDetail</Text>
   }
 }
